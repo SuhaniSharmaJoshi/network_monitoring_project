012657c47acfeb4041d75ca3ae3225d8d6f33799
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-26 12:13:16 -0600

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    default = "us-east-1"
+  
+}
+variable "az" {
+    default = "us-east-1a"
+  
+}
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+variable "key_name" {
+    description = "EC2 key pair name"
+  
+}
