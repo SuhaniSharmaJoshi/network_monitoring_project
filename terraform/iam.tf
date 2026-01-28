ed2d82b1092e00c26b7d54ae3a1b8272aaf65cf8
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-27 18:21:36 -0600

    added iam rule

---
+++ terraform/iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "ec2_role" {
+    name = "ec2-cloudwatch-role"
+
+    assume_role_policy = jsondecode(
+        {
+            Version = "2012-10-17"
+            Statement = [{
+                Effect = "Allow"
+                Principal = {
+                    Service = "ec2.amazonaws.com"
+                }
+                Action = "sts:AssumeRole"
+            }]
+        }
+    )
+
+}
+  resource "aws_iam_instance_profile" "ec2_profile" {
+    name = "ec2-instance-profile"
+    role = aws_iam_role.ec2_role.name
+    
+  }
