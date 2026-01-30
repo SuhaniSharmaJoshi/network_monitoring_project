888d4512e28187c53a7ae297b217e808c77f09df
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-30 13:03:03 -0600

    added cloudwatch agent

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,9 +13,10 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-  #resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
-    
-  #}
+  resource "aws_iam_role_policy_attachment" "cloudwatch_agent_attach" {
+    role = aws_iam_role.ec2_role.name
+    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  }
   resource "aws_iam_instance_profile" "ec2_profile" {
     name = "ec2-instance-profile"
     role = aws_iam_role.ec2_role.name
