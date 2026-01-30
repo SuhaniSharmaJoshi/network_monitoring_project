00af01a9f1cff9b2f95c2b85298b3f61be94c14d
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-01-30 11:39:05 -0600

    added terraform cloud and provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,21 @@
-provider "aws" {
-    region = var.region
-  
+terraform{
+    required_version = ">= 1.5.0"
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+
+    cloud {
+      organization = "SuhaniOrg"
+      workspaces {
+        name = "network_monitoring_project"
+      }
+    }
 }
+  provider "aws" {
+    region = var.region
+    
+  }
