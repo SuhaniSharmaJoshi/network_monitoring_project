bde073f6cf2218a0d4879594c375dd57627603b7
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-02-02 11:22:56 -0600

    added security group to ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "app_server" {
     subnet_id = aws_subnet.public.id
    
     associate_public_ip_address = true
-    
+    vpc_security_group_ids = [aws_security_group.sg_ec2.id]
     tags = {
       Name = "Terraform-EC2-Docker"
     }
