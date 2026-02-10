5443a9525ca2e9185432f622d336bbaf143e9637
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-02-10 11:10:46 -0600

    added iam policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,27 @@
+resource "aws_iam_policy" "cloudwatch_agent_policy" {
+  name        = "CloudWatchAgentPolicy"
+  description = "Permissions for CloudWatch Agent"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams",
+          "logs:DescribeLogGroups",
+          "ec2:DescribeInstances",
+          "ssm:GetParameter"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 resource "aws_iam_role" "ec2_role" {
     name = "ec2-cloudwatch-role"
 
@@ -15,8 +39,10 @@ resource "aws_iam_role" "ec2_role" {
 
   resource "aws_iam_role_policy_attachment" "cloudwatch_agent_attach" {
     role = aws_iam_role.ec2_role.name
-    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  }
+    policy_arn = aws_iam_policy.cloudwatch_agent_policy.arn
+}
+  
+
   resource "aws_iam_instance_profile" "ec2_profile" {
     name = "ec2-instance-profile"
     role = aws_iam_role.ec2_role.name
