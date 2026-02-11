8d5ba67fdfc81587c4b45722f4c906256ea4355b
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-02-10 20:23:37 -0600

    added a lot of continuous integration

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -11,3 +11,71 @@ systemctl enable docker
 usermod -a -G docker ec2-user
 #install git
 dnf install -y git
+#clone Project
+git clone https://github.com/SuhaniSharmaJoshi/network_monitoring_project.git
+cd network_monitoring_project
+cd app
+#build docker image
+docker build -t my-flask-app .
+docker run -d -p 80:5000 --name flask-app my-flask-app
+#install cloud watch agent
+dnf install amazon-cloudwatch-agent -y
+#create cloudwatch confg
+cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "agent": {
+    "metrics_collection_interval": 60,
+    "run_as_user": "root"
+  },
+
+  "metrics": {
+    "aggregation_dimensions": [
+      ["InstanceId"]
+    ],
+    "append_dimensions": {
+      "AutoScalingGroupName": "\${aws:AutoScalingGroupName}",
+      "ImageId": "\${aws:ImageId}",
+      "InstanceId": "\${aws:InstanceId}",
+      "InstanceType": "\${aws:InstanceType}"
+    },
+    "metrics_collected": {
+      "disk": {
+        "measurement": ["used_percent"],
+        "metrics_collection_interval": 60,
+        "resources": ["*"]
+      },
+      "mem": {
+        "measurement": ["mem_used_percent"],
+        "metrics_collection_interval": 60
+      }
+    }
+  },
+
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/secure",
+            "log_group_name": "/ec2/security/ssh",
+            "log_stream_name": "{instance_id}"
+          },
+          {
+            "file_path": "/home/ec2-user/YOUR_REPO/app/app.log",
+            "log_group_name": "/app/flask/logs",
+            "log_stream_name": "{instance_id}"
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
+#start cloudwatch agent
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a fetch-config \
+-m ec2 \
+-c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+-s
+#permissions
+chown -R ec2-user:ec2-user /home/ec2-user
